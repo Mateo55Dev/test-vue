92d53969ae182fc5d8876b39b0786a40ac4d70ff
Mateo08Dev <mateusz.rozenek121@gmail.com>
2023-04-07 12:49:06 +0200

    Update

---
+++ dist/assets/BView-685b2123.js
@@ -0,0 +1,1 @@
+import{L as s}from"./LoremIpsum-eeb68992.js";import{o as a,c as i,d as t,a as e}from"./index-1375a26d.js";const o={class:"BView"},_=e("h1",null,"This is an BView page. This is an BView page. This is an BView page. This is an BView page.",-1),n=e("br",null,null,-1),B={__name:"BView",setup(c){return(r,l)=>(a(),i("div",o,[_,n,t(s)]))}};export{B as default};
